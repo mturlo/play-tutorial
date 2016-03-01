b534d64a738c36ea1930a3792c9a1e94afe26469
Maciej Turlo <maciej.turlo@yougov.com>
2016-03-01 22:07:36 +0100

    returning sample static hardcoded tasks

--- app/controllers/Tasks.scala
+++ app/controllers/Tasks.scala
@@ -1,8 +1,11 @@
 package controllers
 
+import model.Task
 import play.api.data.Forms._
 import play.api.data._
 import play.api.mvc._
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
 
 /**
   * Created by mactur on 01/03/16.
@@ -13,7 +16,13 @@ class Tasks extends Controller {
     "label" -> nonEmptyText
   )
 
-  def listTasks = TODO
+  def listTasks = Action {
+    val sampleTasks = List(
+      Task(1, "Task one"),
+      Task(2, "Task two")
+    )
+    Ok(views.html.tasks(sampleTasks, taskForm))
+  }
 
   def createTask = TODO
 
